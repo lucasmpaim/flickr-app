8c7bbfbe03731b2bdd583ae6af589e1b8497288f
Lucas Paim <lucas.paim@baritecnologia.com.br>
2022-06-18 23:36:51 -0300

    Create FlickrURLBuilder

--- Targets/FlickrService/Tests/FlickrServiceTests.swift
+++ Targets/FlickrService/Tests/FlickrServiceTests.swift
@@ -6,17 +6,26 @@
 //
 
 import XCTest
+import HttpClient
 @testable import FlickrService
 
-//protocol FlickrService {
-//    func
-//}
+enum Flickr {
+    
+}
+
+extension Flickr {
+    enum Error : Swift.Error {
+        case httpError(HTTP.ClientError)
+    }
+}
+
 
 struct RemotePage<T: Decodable>: Decodable {
     let page: UInt
     let pages: UInt
     let perpage: UInt
     let total: UInt
+    let items: [T]
 }
 
 struct RemotoPhoto: Decodable {
@@ -26,10 +35,72 @@ struct RemotoPhoto: Decodable {
     let title: String
 }
 
-struct RemoteResponseMapper {
-    static func 
+struct Page<T> {
+    let page: UInt
+    let totalPages: UInt
+    let perPage: UInt
+    let items: [T]
+}
+
+struct Photo {
+    let id: String
+    let secret: String
+    let isPublic: Bool
+    let title: String
+}
+
+protocol FlickrService {
+    func fetchPopular() async -> Result<Page<Photo>, Flickr.Error>
+}
+
+struct FlickrServiceImpl : FlickrService {
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func fetchPopular() async -> Result<Page<Photo>, Flickr.Error> {
+        return .failure(.httpError(.networkError))
+    }
 }
 
 final class FlickrServiceTests: XCTestCase {
-    
+    func test_whenInvokeFetchPopularMethodShouldReturnPopularPhotos() async {
+//        let stubHttpClient = StubHTTPClient(stub: .failure(.networkError))
+//        let sut = FlickrServiceImpl(client: stubHttpClient)
+//
+//        let result = await sut.fetchPopular()
+//
+//        switch result {
+//        case .success(let page):
+//            XCTAssertTrue(page.items.count > 0)
+//        case .failure(let error):
+//            XCTFail("Expect to receive a page, got \(error) instead")
+//        }
+        
+    }
+}
+
+
+//MARK: - Help Methods
+
+fileprivate extension FlickrServiceTests {
+    final class StubHTTPClient: HTTPClient {
+        
+        private let stubResult: Result<Any, HTTP.ClientError>
+        
+        init(stub: Result<Any, HTTP.ClientError>) {
+            self.stubResult = stub
+        }
+        
+        func getData(from url: URL) async -> Result<Data, HTTP.ClientError> {
+            fatalError("Not implemented")
+        }
+        
+        func getJSON<T: Decodable>(from url: URL, type: T.Type) async -> Result<T, HTTP.ClientError> where T : Decodable {
+            return stubResult as! Result<T, HTTP.ClientError>
+        }
+    }
 }
---
+++ Targets/FlickrService/Tests/FlickrURLBuilderTests.swift
@@ -0,0 +1,59 @@
+//
+//  FlickrURLBuilderTests.swift
+//  FlickrServiceTests
+//
+//  Created by Lucas Paim on 18/06/22.
+//
+
+import Foundation
+import XCTest
+
+
+
+struct FlickrURLBuilder {
+    
+    enum SupportedMethods: String, RawRepresentable {
+        case fetchPopularPhotos = "flickr.photos.getPopular"
+    }
+    
+    private static var apiKey: String {
+        "6ee86dfd9bce6f402e171ff247753cbd"
+    }
+    
+    private static var baseURL: String {
+        "https://www.flickr.com/services/rest/?api_key=\(apiKey)&format=json"
+    }
+    
+    private static var defaultUserNSId: String { "139356341@N05" }
+    
+    private let method: SupportedMethods
+    private var userNSID: String?
+    
+    init(method: SupportedMethods) {
+        self.method = method
+    }
+    
+    mutating func userId(_ id: String) -> Self {
+        self.userNSID = id
+        return self
+    }
+    
+    func build() -> URL? {
+        let userIDString = (userNSID ?? FlickrURLBuilder.defaultUserNSId).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+        let urlString = "\(FlickrURLBuilder.baseURL)&method=\(method.rawValue)&user_id=\(userIDString)"
+        return URL(string: urlString)
+    }
+    
+}
+
+final class FlickrURLBuilderTests: XCTestCase {
+    func test_whenCreateFlickrURL_withPopularPhotosShouldReturnSameURLFromExplorer() {
+        let sut = FlickrURLBuilder(method: .fetchPopularPhotos)
+        let url = sut.build()
+        
+        XCTAssertNotNil(url)
+        XCTAssertEqual(url?.relativeString, "https://www.flickr.com/services/rest/?api_key=6ee86dfd9bce6f402e171ff247753cbd&format=json&method=flickr.photos.getPopular&user_id=139356341%40N05")
+    }
+}
+
+
